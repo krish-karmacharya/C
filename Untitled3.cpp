6567135ed4f3c7af4ab437b02d439f7537976e92
krish-karmacharya <110962590+krish-karmacharya@users.noreply.github.com>
2023-07-13 13:32:30 +0545

    Add files via upload

---
+++ Untitled3.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <conio.h>
+int main ()
+{
+	int a=20;
+	int b=10;
+	int c=5;
+	int d=15;
+	float e1,e2,e3,e4;
+	e1=(a+b)*c/d;
+	e2=((a+b)*c)/d;
+	e3=(a+b)*(c/d);
+	e4=a*b%c/d;
+	return 0;
+}
